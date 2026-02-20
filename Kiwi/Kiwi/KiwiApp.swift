c77b4f9b7ffe908dfb4a6e4593baf1773f2c8706
adiramanan <131528168+adiramanan@users.noreply.github.com>
2026-02-20 19:48:06 +0530

    basic

---
+++ Kiwi/Kiwi/KiwiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiApp.swift
+//  Kiwi
+//
+//  Created by Aditya Rangaramanan on 20/02/26.
+//
+
+import SwiftUI
+
+@main
+struct KiwiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kiwi/KiwiTests/KiwiTests.swift
@@ -0,0 +1,17 @@
+//
+//  KiwiTests.swift
+//  KiwiTests
+//
+//  Created by Aditya Rangaramanan on 20/02/26.
+//
+
+import Testing
+@testable import Kiwi
+
+struct KiwiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
